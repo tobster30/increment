e2c96a2969250f641a9fb26fe16a2e0482f17340
tobster30 <tobster50@outlook.com>
2017-02-27 11:42:59 +0000

    Update IDpanel.java

--- IDpanel.java
+++ IDpanel.java
@@ -1,22 +1,50 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
-public class IDpanel extends JPanel {
-	
-	double total = 50;
-	JButton btnIncrement, btnDecrement;	
+public class IDpanel extends JPanel 
+{
+	private double total;
+	private JButton btnIncrement, btnDecrement;	
+	private JLabel label;
 	
 	public IDpanel()
 	{
 		
+		total = 50;
+		
 		btnIncrement = new JButton("Increment");
 		btnDecrement = new JButton("Decrement");
 		
-		add(btnIncrement);
-		add(btnDecrement);
+		btnIncrement.addActionListener(new ButtonListener());
+		btnDecrement.addActionListener(new ButtonListener());
+
 		
+		label = new JLabel("Total: " + total);
+		
+		add(label);
+		add(btnIncrement);
+		add(btnDecrement);	
 		
 	}
-
+	
+private class ButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			if(event.getSource() == btnIncrement)
+			{
+				total++;
+				label.setText("Pushes: " + total);
+			}
+			else 
+			{
+				total--;
+				label.setText("Pushes: " + total);
+			}   
+		}
+	}
 }
